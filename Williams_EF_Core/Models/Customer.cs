f966be58752b67d237b10a9bf43cc88846e6d0e4
richard williams <richard.williams@student.centralia.edu>
2022-03-16 01:21:55 -0700

    Added Error messages with Data Annotations

--- Williams_EF_Core/Models/Customer.cs
+++ Williams_EF_Core/Models/Customer.cs
@@ -8,8 +8,10 @@ namespace Williams_EF_Core.Models
     {
         public int Id { get; set; }
         [MaxLength(40)]
+        [Required(ErrorMessage = "First Name is required!")]
         public string FirstName { get; set; } = null!;
         [MaxLength(40)]
+        [Required(ErrorMessage = "Last Name is required!")]
         public string LastName { get; set; } = null!;
         [MaxLength(40)]
         public string? City { get; set; }
